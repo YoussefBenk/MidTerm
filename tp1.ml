3fe936125e9e48b5974f143fc43a1df4e36ddee3
Youssef <youssef.benkirane99@gmail.com>
2017-11-02 19:50:03 +0100

    Ex 2 : premmier

--- tp1.ml
+++ tp1.ml
@@ -8,3 +8,17 @@ let reverse nb =
 		else 
 			reverse1 (nb/10) (result*10 + nb mod 10) 
 	in reverse1 nb 0;; 
+
+(* Prime *)
+let prime nb = g
+	if nb < 2 then 
+		invalid_arg "Erreur 1 : Le nombre doit etre superieur a 2"
+	else
+		let rec prime1 n =
+			if n = 1 then 
+				true
+			else 
+				if ((nb mod n) = 0) then 
+					false 
+				else prime1 (n-1) 
+		in prime1 (nb-1)  
